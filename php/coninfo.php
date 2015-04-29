3032ce0ef7c93a8404ffdba5e3ebf56e898f93d2
gregswansonite <georgegarciaweb@gmail.com>
2015-04-29 09:06:08 -0500

    Delete coninfo.php
    this file contains connection information that cannot be public

--- php/coninfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$dbname="customers";
-	$userdbname = "customers";
-	$dbpassword = "customers";
-	$hostname = "23.229.213.195";
-	//SELECT A DATABASE TO WORK WITH 
-	$dbhandle = mysql_connect($hostname, $userdbname, $dbpassword)or die("Could not connect:".mysql_error());	
-	mysql_select_db($dbname,$dbhandle);
-?>
